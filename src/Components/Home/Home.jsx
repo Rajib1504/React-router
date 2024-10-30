e84b329133b2c692c9bb9f416ac0768520993717
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-31 00:35:13 +0530

    useLocation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,9 +1,11 @@
 import Footer from "../Footer/Footer";
 import Head from "../Head/Head";
-import { Outlet, useNavigation } from "react-router-dom";
+import { Outlet, useLocation, useNavigation } from "react-router-dom";
 
 const Home = () => {
   const navigation = useNavigation();
+  const location = useLocation();
+  console.log(location);
   return (
     <div className="  border-2 border-red-700 flex flex-col gap-3 items-center text-xl">
       <Head></Head>
