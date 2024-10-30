62a5590ba780d637858faa21983f774cd68f0476
Rajib1504 <152308020+Rajib1504@users.noreply.github.com>
2024-10-31 00:31:39 +0530

    loading by useNavigation

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import Footer from "../Footer/Footer";
 import Head from "../Head/Head";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 
 const Home = () => {
+  const navigation = useNavigation();
   return (
     <div className="  border-2 border-red-700 flex flex-col gap-3 items-center text-xl">
       <Head></Head>
-      <Outlet></Outlet>
+      {navigation.state === "loading" ? <p>Loading....</p> : <Outlet></Outlet>}
+
       <Footer></Footer>
     </div>
   );
